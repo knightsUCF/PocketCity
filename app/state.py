7858733c7f39ce237b840b62da9fbca8bf14477a
peterknights1 <peterk@knights.ucf.edu>
2017-03-04 18:57:54 -0500

    Create state.py

---
+++ app/state.py
@@ -0,0 +1,18 @@
+class state():
+    def __init__(self):
+        self.PlayerName
+        self.Score
+        self.StartingCapital = 1000
+        self.Capital
+
+
+
+    def StartGame(self):
+        self.Capital = StartingCapital
+        self.Score = 0 
+        return 
+
+    def GameOver(self):
+        return 
+
+
