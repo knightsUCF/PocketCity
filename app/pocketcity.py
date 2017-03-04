ff83031f7ddcc85db8e7fb862ea708308fc6fcc3
peterknights1 <peterk@knights.ucf.edu>
2017-03-04 12:38:43 -0500

    Create pocketcity.py

---
+++ app/pocketcity.py
@@ -0,0 +1,42 @@
+
+'''
+
+double previous = getCurrentTime();
+double lag = 0.0;
+while (true)
+{
+  double current = getCurrentTime();
+  double elapsed = current - previous;
+  previous = current;
+  lag += elapsed;
+
+  processInput();
+
+  while (lag >= MS_PER_UPDATE)
+  {
+    update();
+    lag -= MS_PER_UPDATE;
+  }
+
+  render();
+}
+
+
+'''
+#
+
+
+while (true): 
+  # fix time syncTime()
+  
+  getInput()
+  
+  while (lag >= millisecondsPerUpdate): 
+    update()
+    lag -= millisecondsPerUpdate
+    
+  render()
+
+
+
+
