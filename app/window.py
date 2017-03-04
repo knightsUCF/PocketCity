34eed9de571ba96b1f54b2faae7e99914b0161b4
peterknights1 <peterk@knights.ucf.edu>
2017-03-04 18:59:00 -0500

    Create window.py

---
+++ app/window.py
@@ -0,0 +1,51 @@
+
+import pygame
+import engine 
+
+RunGameLoop = engine.engine()
+
+class window(): 
+    
+    def initializewindow(self):
+        (width, height) = (1200, 700)
+        screen = pygame.display.set_mode((width, height))
+        pygame.display.set_caption('Pocket City')
+        pygame.display.flip()
+
+        RunGameLoop.gameloop()
+
+
+'''
+        
+
+        running = True
+        while running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+                    pygame.quit()
+
+                    '''
+                    
+                    
+
+'''
+    def Update(self):
+        return '''
+
+
+		
+
+
+
+'''
+
+# run the game loop
+while True:
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+
+
+ '''
